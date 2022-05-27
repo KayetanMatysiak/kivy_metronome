2b11220c40673f2527c1aa5eb61a227352ddfba2
Kayetan <kaymatt@live.com>
2022-05-27 10:20:50 +0100

    Sound module changed from Kivy to Pygame. Automatic tempo change/play while using slider.

--- main.py
+++ main.py
@@ -1,41 +1,41 @@
 from kivy.app import App
-from kivy.core.audio import SoundLoader
 from kivy.uix.boxlayout import BoxLayout
-
+import pygame
 
 class Main(BoxLayout):
     click = None
     value = 130
+    pygame.init()
+    metronome_on = False
 
     def __init__(self, **kwargs):
         super(Main, self).__init__(**kwargs)
         self.on_slider_value(self)
 
-    # def on_size(self, *args):
-    #     print(self.width, self.height)
-
     def on_slider_value(self, widget):
         self.value = widget.value
-        return self.value
+        if self.metronome_on is True:
+            pygame.mixer.music.stop()
+            pygame.mixer.music.load(f"sounds/{self.value}.ogg")
+            pygame.mixer.music.play()
 
     def change_icon(self, widget):
         if widget.text == '▶️':
             widget.text = '⏸️'
-            self.start_metronome()
+            self.metronome_on = True
+            self.on_slider_value(self)
         else:
             widget.text = '▶️'
+            self.metronome_on = False
             self.stop_metronome()
 
     def start_metronome(self):
-        # print(self.on_slider_value(self))
-        self.click = SoundLoader.load(f"sounds/{self.on_slider_value(self)}.ogg")
-        self.click.volume = 1.5
-        self.click.play()
+        pygame.mixer.music.stop()
+        pygame.mixer.music.load(f"sounds/{self.on_slider_value(self)}.ogg")
+        pygame.mixer.music.play()
 
     def stop_metronome(self):
-        self.click.stop()
-        self.click.unload()
-        self.click = None
+        pygame.mixer.music.stop()
 
 class Metronome(App):
     pass
